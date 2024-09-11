a8bb5e87c242c159d97084b48707209628b50c05
aliciarolineko <alicia.neko@mail.ru>
2024-09-11 15:16:03 -0400

    Update neko.py

--- neko.py
+++ neko.py
@@ -11,6 +11,7 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
+
 # Configura tu API ID y Hash aquí
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
@@ -33,48 +34,107 @@ async def start(event):
         return
     await event.respond('Funcionando🙃')
 
+compress_in_progress = False
+
 @client.on(events.NewMessage(pattern='/compress'))
 async def compress(event):
+    global compress_in_progress
     sender = await event.get_sender()
     username = sender.username
 
     if username not in allowed_users:
         return
+
+    if compress_in_progress:
+        await event.respond("El comando está en uso actualmente, espere un poco🙃")
+        return
+
     if event.is_reply:
         reply_message = await event.get_reply_message()
         if reply_message.media:
             try:
-                file_path = await client.download_media(reply_message.media)
-                compressed_file = file_path + '.rar'
+                compress_in_progress = True
+                await event.respond("Descargando el archivo para comprimirlo...")
+
+                # Crear carpeta temporal
+                temp_dir = "tempcompress"
+                os.makedirs(temp_dir, exist_ok=True)
+
+                # Descargar archivo
+                file_path = await client.download_media(reply_message.media, file=temp_dir)
+                compressed_file = os.path.join(temp_dir, os.path.basename(file_path) + '.rar')
+
+                await event.respond("Comprimiendo el archivo...")
+
+                # Comprimir archivo
                 with zipfile.ZipFile(compressed_file, 'w') as archive:
                     archive.write(file_path, os.path.basename(file_path))
-                parts = split_file(compressed_file, 10 * 1024 * 1024)
+
+                # Dividir archivo comprimido
+                parts = split_file(compressed_file, 10 * 1024 * 1024, start_index=1)
+                await event.respond(f"Se ha comprimido el archivo en {len(parts)} partes, ahora se enviarán")
+
+                # Enviar partes
                 for part in parts:
                     await client.send_file(event.chat_id, part)
-                os.remove(file_path)
-                os.remove(compressed_file)
+
+                await event.respond("Esas son todas las partes")
+
+                # Limpiar archivos temporales
+                shutil.rmtree(temp_dir)
             except Exception as e:
                 await event.respond(f'Error: {str(e)}')
+            finally:
+                compress_in_progress = False
         else:
             await event.respond('Ejecute el comando respondiendo a un archivo')
     else:
         await event.respond('Ejecute el comando respondiendo a un archivo')
 
+def split_file(file_path, part_size, start_index=1):
+    parts = []
+    with open(file_path, 'rb') as f:
+        part_num = start_index
+        while True:
+            part_data = f.read(part_size)
+            if not part_data:
+                break
+            part_file = f"{file_path}.part{part_num}"
+            with open(part_file, 'wb') as part:
+                part.write(part_data)
+            parts.append(part_file)
+            part_num += 1
+    return parts
+    
+
+command_in_use2 = False
+
 @client.on(events.NewMessage(pattern='/rename (.+)'))
 async def rename(event):
+    global command_in_use2
+
     sender = await event.get_sender()
     username = sender.username
 
     if username not in allowed_users:
         return
+
+    if command_in_use2:
+        await event.respond("El comando está en uso actualmente, espere un poco🙃")
+        return
+
+    command_in_use2 = True
+
     if event.is_reply:
         reply_message = await event.get_reply_message()
         if reply_message.media:
             try:
+                await event.respond("Descargando el archivo para renombrarlo...")
                 new_name = event.pattern_match.group(1)
                 file_path = await client.download_media(reply_message.media)
                 new_file_path = os.path.join(os.path.dirname(file_path), new_name)
                 os.rename(file_path, new_file_path)
+                await event.respond("Subiendo el archivo con nuevo nombre...")
                 await client.send_file(event.chat_id, new_file_path, force_document=True)
                 os.remove(new_file_path)
             except Exception as e:
@@ -83,6 +143,8 @@ async def rename(event):
             await event.respond('Ejecute el comando respondiendo a un archivo')
     else:
         await event.respond('Ejecute el comando respondiendo a un archivo')
+
+    command_in_use = False
 
 def split_file(file_path, part_size):
     parts = []
@@ -98,57 +160,80 @@ def split_file(file_path, part_size):
             parts.append(part_file)
             part_num += 1
     return parts
+
     
+h3_in_use = False
+
+def clean_string(input_string):
+    return ''.join(char for char in input_string if char.isalnum() or char in '[]')
+
 @client.on(events.NewMessage(pattern='/h3dl ?(.*)'))
 async def download_images(event):
+    global h3_in_use
+    if h3_in_use:
+        await event.reply("El comando está en uso actualmente, espere un poco🙃")
+        return
+
+    h3_in_use = True
     sender = await event.get_sender()
     username = sender.username
-    code = event.pattern_match.group(1)
+    codes = event.pattern_match.group(1).split(',')
 
-    if not code:
+    if not codes:
         await event.reply("No puedes enviar el comando vacío")
+        h3_in_use = False
         return
 
-    url = f"https://es.3hentai.net/d/{code}"
+    total_codes = len(codes)
+    for index, code in enumerate(codes, start=1):
+        code = clean_string(code.strip())
+        url = f"https://es.3hentai.net/d/{code}"
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        await event.reply(f"Error al acceder a la página: {str(e)}")
-        return
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-    page_title = soup.title.string if soup.title else "sin_titulo"
-    folder_name = re.sub(r'[\\/*?:"<>|]', "", page_title)
-
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-
-    image_links = []
-    for img in soup.find_all('img'):
-        src = img.get('src')
-        if src and src.endswith('t.jpg'):
-            image_links.append(src.replace('t.jpg', '.jpg'))
-
-    for link in image_links:
         try:
-            img_data = requests.get(link).content
-            img_name = os.path.join(folder_name, os.path.basename(link))
-            with open(img_name, 'wb') as handler:
-                handler.write(img_data)
-        except Exception as e:
-            await event.reply(f"Error al descargar el archivo {link}: {str(e)}")
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            await event.reply(f"Error al acceder a la página: {str(e)}")
+            h3_in_use = False
             return
 
-    zip_filename = f"{folder_name}.cbz"
-    with zipfile.ZipFile(zip_filename, 'w') as zipf:
-        for root, _, files in os.walk(folder_name):
-            for file in files:
-                zipf.write(os.path.join(root, file), arcname=file)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        page_title = soup.title.string if soup.title else "sin_titulo"
+        folder_name = clean_string(re.sub(r'[\\/*?:"<>|]', "", page_title))
 
-    await client.send_file(event.chat_id, zip_filename)
-    await event.reply("Archivo CBZ enviado correctamente")
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+
+        image_links = []
+        for img in soup.find_all('img'):
+            src = img.get('src')
+            if src and src.endswith('t.jpg'):
+                image_links.append(src.replace('t.jpg', '.jpg'))
+
+        for link in image_links:
+            try:
+                img_data = requests.get(link).content
+                img_name = os.path.join(folder_name, os.path.basename(link))
+                with open(img_name, 'wb') as handler:
+                    handler.write(img_data)
+                await event.reply(f"Descargando {code} (Progreso {index}/{total_codes})")
+            except Exception as e:
+                await event.reply(f"Error al descargar el archivo {link}: {str(e)}")
+                h3_in_use = False
+                return
+
+        zip_filename = f"{folder_name}.cbz"
+        with zipfile.ZipFile(zip_filename, 'w') as zipf:
+            for root, _, files in os.walk(folder_name):
+                for file in files:
+                    zipf.write(os.path.join(root, file), arcname=file)
+
+        await client.send_file(event.chat_id, zip_filename)
+        await event.reply(f"Archivo {code} descargado, enviando...")
+
+    await event.reply("Todos los archivos CBZ han sido enviados correctamente")
+    h3_in_use = False
+
 
 
 
@@ -169,8 +254,11 @@ async def set_mail(event):
     user_emails[username] = email
     await event.reply(f"Correo electrónico {email} registrado para el usuario @{username}")
 
+sendmail_in_use = False
+
 @client.on(events.NewMessage(pattern='/send'))
 async def send_mail(event):
+    global sendmail_in_use
     sender = await event.get_sender()
     username = sender.username
 
@@ -179,6 +267,10 @@ async def send_mail(event):
 
     if username not in user_emails:
         await event.reply("Use /setmail para registrar su correo")
+        return
+
+    if sendmail_in_use:
+        await event.reply("Se está enviando un correo actualmente, espere un poco🙃")
         return
 
     recipient_email = user_emails[username]
@@ -190,6 +282,8 @@ async def send_mail(event):
             await event.reply("Debe responder a un mensaje")
             return
 
+        sendmail_in_use = True
+
         # Conectar al servidor SMTP de Disroot
         try:
             server = smtplib.SMTP('disroot.org', 587)
@@ -198,15 +292,17 @@ async def send_mail(event):
 
             # Crear el correo
             msg = MIMEMultipart()
-            msg['From'] = DISMAIL
+            msg['From'] = "Neko bot <" + DISMAIL + ">"
             msg['To'] = recipient_email
             msg['Subject'] = "Mensaje desde Telegram"
 
             if reply_message.media:
                 # Si el mensaje tiene un archivo adjunto
+                await event.reply("Descargando adjunto...")
                 file = await reply_message.download_media()
                 if os.path.getsize(file) > 11 * 1024 * 1024:  # 11MB en bytes
-                    await event.reply("No puedes enviar este archivo, debe ser menor a 11MB")
+                    await event.reply("El archivo no debe superar los 11MB, use /compress para reducir el volumen")
+                    sendmail_in_use = False
                     return
 
                 attachment = open(file, 'rb')
@@ -219,17 +315,21 @@ async def send_mail(event):
                 msg.attach(part)
             else:
                 await event.reply("Solo se pueden enviar archivos, videos, audios y fotos, no texto.")
+                sendmail_in_use = False
                 return
 
             # Enviar el correo
             server.sendmail(DISMAIL, recipient_email, msg.as_string())
             server.quit()
 
-            await event.reply("Mensaje enviado correctamente")
+            await event.reply("Correo electrónico enviado correctamente")
         except Exception as e:
             await event.reply(f"Error al enviar el mensaje: {str(e)}")
+        finally:
+            sendmail_in_use = False
     else:
         await event.reply("Debe responder a un mensaje")
+
                   
     
 client.start()
