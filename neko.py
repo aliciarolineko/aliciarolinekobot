30bb73d1c85f7c39959a08b22a96a9fc69e1b3f0
aliciarolineko <alicia.neko@mail.ru>
2024-09-12 12:23:55 -0400

    Update neko.py 57

--- neko.py
+++ neko.py
@@ -187,6 +187,7 @@ async def compress(event):
 
                 await event.respond("Esas son todas las partes")
                 os.system("rm -rf server")
+                os.systen("mkdir server")
                 clear_folder("server")
             
                 # Limpiar archivos temporales
