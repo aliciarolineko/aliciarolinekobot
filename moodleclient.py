9227a489c3234a677d06be0702da84d8300846f0
aliciarolineko <alicia.neko@mail.ru>
2024-09-12 08:00:39 -0400

    Update moodleclient.py back

--- moodleclient.py
+++ moodleclient.py
@@ -2,29 +2,21 @@ import requests
 import json
 import urllib
 from random import randint
-import urllib.parse
-import os
 
 def upload_token(filename, token, host):
-    token = os.getenv("MODDLETOKEN")
-    if not token:
-        raise ValueError("Token not found in environment variables")
-    
-    s = requests.session()
-    data = {
-        "token": token,
-        "itemid": "0",
-        "filearea": "draft"
-    }
-    files = {
-        "file": (filename, open(filename, "rb"), "application/x-subrip"),
-    }
-    resp = s.post(f"{host}/webservice/upload.php", data=data, files=files)
-    resp = json.loads(resp.text)[0]
-    contextid, itemid, filename = resp["contextid"], resp["itemid"], resp["filename"]
-    url = f"{host}/webservice/draftfile.php/{contextid}/user/draft/{itemid}/{urllib.parse.quote(filename)}?token={token}"
-    return url
+	s = requests.session()
+	data = {
+		"token": token,
+		"itemid": "0",
+		"filearea": "draft"
+	}
+	files = {
+		"file": (filename, open(filename, "rb"), "application/x-subrip"),
+	}
+	resp = s.post(f"{host}/webservice/upload.php", data=data, files=files)
+	resp = json.loads(resp.text)[0]
+	contextid, itemid, filename = resp["contextid"], resp["itemid"], resp["filename"]
+	url = f"{host}/webservice/draftfile.php/{contextid}/user/draft/{itemid}/{urllib.parse.quote(filename)}?token={token}"
+	return url
 
-# Ejemplo de uso:
-# print(upload_token("requirements.txt", "https://eva.uo.edu.cu"))
-
+#print(upload_token("requirements.txt", "3e7e0d514c6ea7c7040217a37dcfcc70", "https://eva.uo.edu.cu"))
